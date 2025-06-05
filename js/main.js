4e0d58629815d736914f0f7902a40dc7fb32597b
lynn.972272 <lynn.9702272@gmail.com>
2025-06-05 18:13:18 +0900

    [docs] js 연동 및 swiper 연동 진행중

---
+++ js/main.js
@@ -0,0 +1,14 @@
+window.addEventListener("DOMContentLoaded", function () {
+  // main
+  const main = this.document.querySelector(".main");
+  // 윈도우 스크롤 체크
+  window.addEventListener("scroll", function () {
+    const scY = window.scrollY;
+    if (scY > 0) {
+      console.log("스크롤 됨");
+    } else {
+      main.classList.remove("main_scroll");
+    }
+  });
+});
+
